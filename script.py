31ecbbee46ab153781dc411d822af15526048853
Remco Wolvenne <r.wolvenne@programic.com>
2022-08-02 16:42:08 +0200

    Cleanup.

--- script.py
+++ script.py
@@ -1,30 +1,32 @@
 import os
 from slack_cleaner2 import *
 
-s = SlackCleaner(os.getenv('SLACK_TOKEN'))
+def print_stuff(s):
+    print('users', s.users)
+    print('public channels', s.channels)
+    print('private channels', s.groups)
+    print('instant messages', s.ims)
+    print('multi user direct messages', s.mpim)
 
-def print_stuff():
-  print('users', s.users)
-  print('public channels', s.channels)
-  print('private channels', s.groups)
-  print('instant messages', s.ims)
-  print('multi user direct messages', s.mpim)
+def delete_from_channel(s):
+    for msg in s.c['bot-alerts-red'].msgs():
+        msg.delete(replies=True, files=True)
 
-def delete_from_channel():
-  for msg in s.c['bot-alerts-red'].msgs():
-    msg.delete(replies=True, files=True)
+def delete_from_ims(s):
+    for c in s.ims:
+        if c.name == 'j.doe':
+            for msg in c.msgs():
+                if msg.user == s.myself:
+                    msg.delete(replies=True, files=True)
 
-def delete_from_ims():
-  for c in s.ims:
-    if c.name == 'j.doe':
-      for msg in c.msgs():
-        if msg.user == s.myself:
-          msg.delete(replies=True, files=True)
+def main():
+    s = SlackCleaner(os.getenv('SLACK_TOKEN'))
 
-print_stuff()
+    print_stuff(s)
+    # delete all messages from a channel
+#     delete_from_channel(s)
+    # Delete all messages between you and another user (ims)
+#     delete_from_ims(s)
 
-# delete all messages from a channel
-# delete_from_channel()
-
-# Delete all messages between you and another user (ims)
-# delete_from_ims()
+if __name__ == '__main__':
+    main()
